ba3b921829435d8e0221f6801f531a85de6cdca5
Gheorghita Stefana <gheorghitastefana@gmail.com>
2023-12-15 23:38:45 +0200

    Adding middle scene

---
+++ IMR Proiect/Assets/_ProjectAssets/Scripts/HubScene/Portal.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    public Transform player; 
+    public float proximityDistance = 1.5f; 
+
+    public string sceneToLoad;
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.position);
+        if (distance < proximityDistance)
+        {
+
+            LoadScene();
+        }
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
